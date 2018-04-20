335b257ae0c7a326d9c8fa0078701c4a55962347
Vadym Borys <bublik.drdrdr@gmail.com>
2018-04-20 22:22:43 +0200

    generated template

---
+++ src/main/java/tk/ubublik/pai/PaiApplication.java
@@ -0,0 +1,12 @@
+package tk.ubublik.pai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaiApplication.class, args);
+	}
+}
